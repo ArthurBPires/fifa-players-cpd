0e945447bf2fe6ca955d6df4aba9ac0983385aef
Arthur Brackmann Pires <73498119+ArthurBPires@users.noreply.github.com>
2021-10-24 23:35:29 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+/**
+ * Aplicação dividida em 2 fases:
+ * - Fase 1: Construção e inicialização das estruturas necessárias para suportar as consultas. Max 3 min de duração, tentar < 1min.
+ * - Fase 2: Permite a busca e exibição de informações. Nesta fase os arquivos não devem ser abertos.
+ * - Pesquisa por Nome (player <name or prefix>) : $ player Fer
+ * - Pesquisa por avaliações de usuário (user <userID>) : $ user 4
+ * - Pesquisa por melhores jogadores um posição x (top<N> ‘<position>’): $ top10 ‘ST’
+ * - Pesquisa por tags (tags <list of tags>): $ tags ‘Brazil’ ‘Dribbler’
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+int main() {
+  return 0;
+}
